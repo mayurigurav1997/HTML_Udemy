4a066fbe5b42786f23294acf2a67863d274c3028
Mayuri Gurav <mayurigurav1997@gmail.com>
2022-11-07 11:26:33 +0530

    Scroll to the top

--- 09-Omnifood-Optimizations/script.js
+++ 09-Omnifood-Optimizations/script.js
@@ -12,6 +12,23 @@ btnNavE1.addEventListener("click", function () {
 })
 
 
+// Scroll to the top
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    // e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      })
+    }
+  })
+})
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 
